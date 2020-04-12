bdeb12663b5395c1c4f0c25f303dbef051f60f91
NicholasZeitoun <nzeit8003@gmail.com>
2020-04-12 21:32:26 +1000

    Add steps functionality to new recipe process

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,4 +1,33 @@
 class StepsController < ApplicationController
+  before_action :set_recipe, only: [:new, :show, :create]
+
   def new
+    @step = Step.new
+  end
+
+  def create
+    @step = Step.new(step_params)
+    @step.recipe = @recipe
+
+    # if came_from_update_page? # when updating ingredient
+      # @dose.save
+      # redirect_to edit_cocktail_path(@cocktail)
+    # else # will redirect to cocktail dose create page
+      if @step.save
+        redirect_to new_recipe_step_path(@recipe)
+      else
+        render :new
+      end
+    # end
+  end
+
+  private
+
+  def step_params
+    params.require(:step).permit(:description)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_101034) do
+ActiveRecord::Schema.define(version: 2020_04_12_112125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2020_04_12_101034) do
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
+  create_table "steps", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "recipe_id"
+    t.string "description"
+    t.index ["recipe_id"], name: "index_steps_on_recipe_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,4 +57,5 @@ ActiveRecord::Schema.define(version: 2020_04_12_101034) do
 
   add_foreign_key "ingredients", "recipes"
   add_foreign_key "recipes", "users"
+  add_foreign_key "steps", "recipes"
 end
